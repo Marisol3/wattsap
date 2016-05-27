e41423110b9bffef83d20b11558e8098170d63da
Erika Inoñan Labrin <erika.janet.inonan@gmail.com>
2016-05-27 10:39:11 -0500

    Terminado

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var io=require('socket.io')(http);
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
-
+app.use(express.static('public'));
 io.on('connection',function(socket){
-	//console.log("Un nuevo usuario");
+	console.log("Un nuevo usuario");
 	socket.on('chat', function(_msg){
 		io.emit('nuevo_mensaje',_msg);
 	});
